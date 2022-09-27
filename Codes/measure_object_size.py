425fab160339ee55e8ec6b20e8ccf395e04998dc
yoonseok0323 <yoonrock02@gmail.com>
2022-09-27 17:05:03 +0900

    update

--- Codes/measure_object_size.py
+++ Codes/measure_object_size.py
@@ -11,62 +11,58 @@ aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
 detector = HomogeneousBgDetector()
 
 # Load Image
-img1 = cv2.imread("/Users/krc/Downloads/measure_object_size/imgs/box with marker1.png")
-img2 = cv2.imread("/Users/krc/Downloads/measure_object_size/imgs/box with marker2.png")
+# img = cv2.imread("phone_aruco_marker1.jpg")
+img = cv2.imread("/Users/krc/Downloads/measure_object_size/output1.png")
 
-def get_sizes(image):
-    # Get Aruco marker
-    corners, _, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
+# Get Aruco marker
+corners, _, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
 
-    # Draw polygon around the marker
-    int_corners = np.int0(corners)
-    cv2.polylines(image, int_corners, True, (0, 255, 0), 5)
+# Draw polygon around the marker
+int_corners = np.int0(corners)
+cv2.polylines(img, int_corners, True, (0, 255, 0), 5)
 
-    # Aruco Perimeter
-    aruco_perimeter = cv2.arcLength(corners[0], True)
+# Aruco Perimeter
+aruco_perimeter = cv2.arcLength(corners[0], True)
 
-    # Pixel to cm ratio
-    pixel_cm_ratio = aruco_perimeter / 20
+# Pixel to cm ratio
+pixel_cm_ratio = aruco_perimeter / 20
 
-    contours = detector.detect_objects(image)
+contours = detector.detect_objects(img)
 
-    # Draw objects boundaries
-    height_lists = [] 
-    width_lists = []
-    for cnt in contours:
-        # Get rect
-        rect = cv2.minAreaRect(cnt)
-        (x, y), (h, w), angle = rect
+# 가로,세로 list 생성
+height_list = []
+width_list = []
 
-        # Get Width and Height of the Objects by applying the Ratio pixel to cm
-        object_height = h / pixel_cm_ratio
-        object_width = w / pixel_cm_ratio
-        r_height = round(object_height,1)
-        r_width = round(object_width,1)
-        height_lists.append(r_height)
-        width_lists.append(r_width)
+# Draw objects boundaries
+# 모든 점의 배열 추출
+for cnt in contours:
+    # Get rect
+    rect = cv2.minAreaRect(cnt)
+    (x, y), (w, h), angle = rect
 
-    print(max(height_lists), max(width_lists)) 
+    # Get Width and Height of the Objects by applying the Ratio pixel to cm
+    object_width = w / pixel_cm_ratio
+    object_height = h / pixel_cm_ratio
+    height_list.append(h/pixel_cm_ratio)
+    width_list.append(w/pixel_cm_ratio)
+    
+    print(object_height)
+    print(object_width)
+    print("가로",max(height_list))
+    print("세로",max(width_list))
 
-    # Display rectangle
-    # box = cv2.boxPoints(rect)
-    # box = np.int0(box)
-    # cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), -1)
-    # cv2.polylines(image, [box], True, (255, 0, 0), 2)
-    # cv2.putText(image, "Height {} cm".format(max(height_lists)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-    # cv2.putText(image, "Width {} cm".format(max(width_lists)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-    # # cv2.imwrite('new.jpg',image)
-    # cv2.imshow("Image", image)
-    # cv2.waitKey(0)
+        # Display rectangle
 
-    # return max(height_lists), max(width_lists)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
 
-def compare_width(img1_width, img2_width):
-    if img1_width - img2_width < 0.5: 
-        print(img1_height, img1_width, img2_height) 
+cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
+cv2.polylines(img, [box], True, (255, 0, 0), 2)
+cv2.putText(img, "Height {} cm".format(round(max(height_list))), (int(x-50 ), int(y+52)), cv2.FONT_HERSHEY_PLAIN, 5, (100, 200, 1), 10)
+cv2.putText(img, "Width {} cm".format(round(max(width_list))), (int(x-50), int(y -55)), cv2.FONT_HERSHEY_PLAIN, 5, (500, 200, 1), 10)
 
-# img1_height, img1_width = get_sizes(img1)
-# img2_height, img2_width = get_sizes(img2)
-# compare_width(img1_width, img2_width)   
-get_sizes(img1)
-get_sizes(img2)
+
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.imwrite('/Users/krc/Downloads/measure_object_size/Img_save/test2.png',img)
