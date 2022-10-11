ec6ce8a8ce1c04957118a5f639e77ffce46a8309
Jayh625 <104330432+Jayh625@users.noreply.github.com>
2022-10-11 13:33:59 +0900

    10/11 Updated Github

--- measure_object_size.py
+++ measure_object_size.py
@@ -3,23 +3,20 @@ from object_detector import *
 import numpy as np
 from rembg import remove
 import os 
-parameters = cv2.aruco.DetectorParameters_create()
-aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+
 detector = Measuring_Volume()
 
-img_path = "/Users/krc/Downloads/Fit_in_Volume/imgs"
-input = f"{img_path}/물병1.jpg"
-name = os.path.basename(input)
-input_path = f'/Users/krc/Downloads/Fit_in_Volume/imgs/{name}'
-output_path = f'/Users/krc/Downloads/Fit_in_Volume/rembg/Rembg_{name}'
+# input = f'C:/Users/Jay/Downloads/Fit_in_Volume/imgs/크라운산도측면.jpg'
+# name = os.path.basename(input)
 
-input = cv2.imread(input_path)
-output = remove(input)
-cv2.imwrite(output_path, output)
+img_dir = 'C:/Users/Jay/Downloads/Fit_in_Volume/uploads'
+files = os.listdir(img_dir)
 
-img = cv2.imread(output_path)
-
-def get_sizes(image):
+def get_sizes(input,name):
+    image = cv2.imread(input)
+    parameters = cv2.aruco.DetectorParameters_create()
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)
+ 
     corners, _, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
 
     int_corners = np.int0(corners)
@@ -27,12 +24,16 @@ def get_sizes(image):
     cv2.polylines(image, int_corners, True, (0, 255, 0), 5)
     
     aruco_perimeter = cv2.arcLength(corners[0], True)
-
+    
     pixel_cm_ratio = aruco_perimeter / 16
-  
-    contours = detector.detect_objects(image)
-    height_lists = [] 
-    width_lists = []
+
+    rembg_path = f'C:/Users/Jay/Downloads/Fit_in_Volume/rembg/Rembg_{name}'
+    output = remove(image)
+    cv2.imwrite(rembg_path, output)
+    img = cv2.imread(rembg_path)
+
+    contours = detector.detect_objects(img)
+
     for cnt in contours:
         rect = cv2.minAreaRect(cnt)
         (x, y), (h, w), angle = rect
@@ -41,27 +42,17 @@ def get_sizes(image):
         object_width = w / pixel_cm_ratio
         r_height = round(object_height,1)
         r_width = round(object_width,1)
-        height_lists.append(r_height)
-        width_lists.append(r_width)
-
+        
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-    cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), -1)
+    cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), -1) 
     cv2.polylines(image, [box], True, (255, 0, 0), 2)
-    cv2.putText(image, "Height {} cm".format(r_width), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 400, 0), 4)
-    cv2.putText(image, "Width {} cm".format(r_height), (int(x - 100), int(y + 50)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 400, 0), 4)
+    cv2.putText(image, "Height {} cm".format(r_height), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 400, 0), 4)
+    cv2.putText(image, "Width {} cm".format(r_width), (int(x - 100), int(y + 50)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 400, 0), 4)
     cv2.imshow("Image", image)
-    cv2.imwrite(f"/Users/krc/Downloads/Fit_in_Volume/output/Output_{name}", image)
+    cv2.imwrite(f"C:/Users/Jay/Downloads/Fit_in_Volume/output/Output_{name}", image)
     cv2.waitKey(0)
 
-# def compare_width(img1_width, img2_width):
-#     if img1_width - img2_width < 0.5: 
-#         print(img1_height, img1_width, img2_height) 
-
-# img1_height, img1_width = get_sizes(img1)
-# img2_height, img2_width = get_sizes(img2)
-# compare_width(img1_width, img2_width)   
-# get_sizes(img1)
-# get_sizes(img2)
-
-get_sizes(img)
+for item in files:
+    if (img_dir+item).__contains__(".jpg" or ".png"):
+        get_sizes(img_dir + item, item)
--- prac.py
+++
@@ -1,4 +0,0 @@
-import os
-input = "/Users/krc/Downloads/Fit_in_Volume/imgs/노트북.jpg"
-name = os.path.basename(input)
-print(name)
