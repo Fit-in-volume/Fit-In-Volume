0167b362e0c87636ec6d4bb9fed6f810f7eed4e6
Jayh625 <jayho625@gmail.com>
2022-10-21 12:02:26 +0900

    Changed docs and imgs on 10/21

---
+++ docs/other codes/detect_video.py
@@ -0,0 +1,92 @@
+import cv2
+from object_detector import *
+import numpy as np
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+# Load Aruco detector
+parameters = cv2.aruco.DetectorParameters_create()
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+
+
+# Load Object Detector
+detector = HomogeneousBgDetector()
+
+# Load Cap
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+while True:
+    _, img = cap.read()
+
+    # Get Aruco marker
+    corners, _, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+    if corners:
+
+        # Draw polygon around the marker
+        int_corners = np.int0(corners)
+        cv2.polylines(img, int_corners, True, (0, 255, 0), 5)
+
+        # Aruco Perimeter
+        aruco_perimeter = cv2.arcLength(corners[0], True)
+
+        # Pixel to cm ratio
+        pixel_cm_ratio = aruco_perimeter / 16
+
+        contours = detector.detect_objects(img)
+        
+        height_lists = [] 
+        width_lists = []
+        # Draw objects boundaries
+        for cnt in contours:
+            # Get rect
+            rect = cv2.minAreaRect(cnt)
+            (x, y), (w, h), angle = rect
+
+            # Get Width and Height of the Objects by applying the Ratio pixel to cm
+            object_width = w / pixel_cm_ratio
+            object_height = h / pixel_cm_ratio
+            r_height = round(object_height,1)
+            r_width = round(object_width,1)
+
+        # Display rectangle
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+
+            cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
+            cv2.polylines(img, [box], True, (255, 0, 0), 2)
+            cv2.putText(img, "Height {} cm".format(r_height), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 200, 0), 2)
+            cv2.putText(img, "Width {} cm".format(r_width), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 4, (100, 200, 0), 2)
+
+
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
